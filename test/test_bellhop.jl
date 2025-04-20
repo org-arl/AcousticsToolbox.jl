4e6e6bdf573aeae98bfa5ce46a80d53ec77557c3
Mandar Chitre <mandar@nus.edu.sg>
2025-04-20 23:14:26 +0800

    test: add Kraken test cases

--- test/test_bellhop.jl
+++ test/test_bellhop.jl
@@ -1,16 +1,5 @@
 using TestItems
 
-@testitem "models" begin
-  using UnderwaterAcoustics
-  m = models()
-  @test m isa Vector{Type{<:UnderwaterAcoustics.AbstractPropagationModel}}
-  @test Bellhop ∈ m
-  env = UnderwaterEnvironment(bathymetry = 20.0u"m", temperature = 27.0u"°C", salinity = 35.0u"ppt", seabed = SandySilt)
-  m = models(env)
-  @test m isa Vector{Type{<:UnderwaterAcoustics.AbstractPropagationModel}}
-  @test Bellhop ∈ m
-end
-
 @testitem "bellhop-pekeris-rays+ir" begin
   using UnderwaterAcoustics
   env = UnderwaterEnvironment(bathymetry = 20.0u"m", temperature = 27.0u"°C", salinity = 35.0u"ppt", seabed = SandySilt)
