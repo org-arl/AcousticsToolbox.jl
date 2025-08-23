4a66fd2711d2aa2d875f8c945ee0d1584df51a9b
Mandar Chitre <mandar@nus.edu.sg>
2025-08-24 02:45:18 +0800

    fix(kraken): leave larger margin for errors in group velocity

--- src/kraken.jl
+++ src/kraken.jl
@@ -58,11 +58,11 @@ function UnderwaterAcoustics.arrivals(pm::Kraken, tx1::AbstractAcousticSource, r
     if pm.env.seabed isa MultilayerElasticBoundary
       # increase depth to include sediment layers
       D += sum(l -> l.h, pm.env.seabed.layers[1:end-1])
-      max_c = max(max_c, maximum(l -> max(maximum(l.cₚ), maximum(l.cₛ)), pm.env.seabed.layers[1:end-1]))
+      max_c = 2 * max(max_c, maximum(l -> max(maximum(l.cₚ), maximum(l.cₛ)), pm.env.seabed.layers[1:end-1]))
     elseif pm.env.seabed isa ElasticBoundary
-      max_c = max(max_c, maximum(pm.env.seabed.cₚ), maximum(pm.env.seabed.cₛ))
+      max_c = 2 * max(max_c, maximum(pm.env.seabed.cₚ), maximum(pm.env.seabed.cₛ))
     else
-      max_c = max(max_c, maximum(pm.env.seabed.c))
+      max_c = 2 * max(max_c, maximum(pm.env.seabed.c))
     end
     rxs = AcousticReceiverGrid2D(rx1.pos.x, range(-D, 0; length=ceil(Int, 10D/λ + 1)))
     _write_env(pm, [tx1], rxs, dirname)
